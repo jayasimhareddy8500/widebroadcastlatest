9fd036395af72f4ed4cfbdbe5d72728822712cbc
jayasimhareddy8500 <47518680+jayasimhareddy8500@users.noreply.github.com>
2020-02-18 18:18:20 +0530

    Update Slot.java

--- src/main/java/com/widebroadcast/entity/Slot.java
+++ src/main/java/com/widebroadcast/entity/Slot.java
@@ -12,7 +12,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OrderBy;
-import javax.persistence.Version;
 
 import lombok.Data;
 
